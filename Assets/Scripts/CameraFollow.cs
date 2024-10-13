93c0343b3d5237c8bbc64f40b46ae12510564138
David Madi <david.madi@points.com>
2024-10-12 23:11:34 -0400

    clouds

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -8,14 +8,17 @@ using UnityEngine.InputSystem.Controls;
 public class CameraFollow : MonoBehaviour
 {
     public GameObject Follow;
-    public GameObject Wall;
+    public GameObject WallBack;
+    public GameObject WallFront;
     private Vector3 cameraDistance;
-    private Vector3 wallDistance;
-    private MeshRenderer wallMeshRenderer;
+    private Vector3 wallBackDistance;
+    private Vector3 wallFrontDistance;
+    private MeshRenderer wallBackMeshRenderer;
+    private MeshRenderer wallFrontMeshRenderer;
     // Start is called before the first frame update
     private float followYOffset;
     private Vector3 cameraMoveDestination;
-    private Vector2 wallMoveDestination;
+    // private Vector2 wallMoveDestination;
     private int cameraMoveSpeed = 5;
     //////////// same memory position
     private float verticalMoveCameraY;
@@ -26,8 +29,10 @@ public class CameraFollow : MonoBehaviour
     {
       this.cameraDistance = Follow.transform.position - this.transform.position;
       this.followYOffset =  this.transform.position.y - Follow.transform.position.y;
-      this.wallDistance = this.Wall.transform.position - this.transform.position;
-      this.wallMeshRenderer = Wall.GetComponent<MeshRenderer>();
+      this.wallBackDistance = this.WallBack.transform.position - this.transform.position;
+      this.wallFrontDistance = this.WallFront.transform.position - this.transform.position;
+      this.wallBackMeshRenderer = WallBack.GetComponent<MeshRenderer>();
+      this.wallFrontMeshRenderer = WallFront.GetComponent<MeshRenderer>();
       UpdateFollow(Follow);
     }
 
@@ -44,14 +49,17 @@ public class CameraFollow : MonoBehaviour
       }
       this.transform.position = new Vector3(this.transform.position.x, this.verticalMoveCameraY, this.transform.position.z);
       //Wall follow
-      this.Wall.transform.position = this.transform.position + this.wallDistance;
+      // this.WallBack.transform.position = this.transform.position + this.wallBackDistance;
+      this.WallBack.transform.position = new Vector3(this.WallBack.transform.position.x, this.transform.position.y, this.WallBack.transform.position.z);
+      this.WallFront.transform.position = new Vector3(this.WallFront.transform.position.x, this.transform.position.y, this.WallFront.transform.position.z);
       //Roll wall texture 
-      wallMeshRenderer.materials[0].mainTextureOffset = new Vector2(this.Wall.transform.position.x / -100, this.Wall.transform.position.y / -100);
+      wallBackMeshRenderer.materials[0].mainTextureOffset = new Vector2(this.WallBack.transform.position.x / -100, this.WallBack.transform.position.y / -100);
+      wallFrontMeshRenderer.materials[0].mainTextureOffset = new Vector2(this.WallFront.transform.position.x / -100, this.WallFront.transform.position.y / -100);
     }
 
     public void UpdateFollow(GameObject toFollow) {
       this.Follow = toFollow;
       this.cameraMoveDestination = new Vector3(Follow.transform.position.x, Follow.transform.position.y + followYOffset, this.transform.position.z);
-      this.wallMoveDestination = new Vector2(this.transform.position.x / -100, Follow.transform.position.y / -100);
+      // this.wallMoveDestination = new Vector2(this.transform.position.x / -100, Follow.transform.position.y / -100);
     } 
 }
